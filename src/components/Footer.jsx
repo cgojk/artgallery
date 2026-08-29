f8dd5c166111b46414b8253926013e5ba427450e
catalina <catalinaruedaf@hotmail.com>
2026-08-30 06:44:31 +1000

    adding the styles for the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,31 +3,34 @@ import facebookIcon from "../assets/icon-facebook.svg";
 import instagramIcon from "../assets/icon-instagram.svg";
 
 import twitterIcon from "../assets/icon-twitter.svg";
-import pinterestIcon from "../assets/icon-pinterest.svg";
+
+
 
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__container">
-        <h1 className="footer__title">Modern art gallery</h1>
+        <div className="footer__container container">
+            <div className="footer__info ">
+                <h1 className="footer__title">Modern art gallery</h1>
 
-        <p className="footer__text">The Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9am.  Find us at 99 King Street, Newport, USA</p>
+                <p className="footer__text">The Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9am.  Find us at 99 King Street, Newport, USA</p>
 
-      </div>
-        <div className="footer__container--icons">
-          <a href="#" className="footer__icon">
-            <img src={facebookIcon} alt="Facebook" />
-          </a>
-          <a href="#" className="footer__icon">
-            <img src={instagramIcon} alt="Instagram" />
-          </a>
-          <a href="#" className="footer__icon">
-            <img src={twitterIcon} alt="Twitter" />
-          </a>
-          <a href="#" className="footer__icon">
-            <img src={pinterestIcon} alt="Pinterest" />
-          </a>
+            </div>
+                <div className="footer__container--icons">
+                <a href="#" className="footer__icon">
+                    <img src={facebookIcon} alt="Facebook" />
+                </a>
+                <a href="#" className="footer__icon">
+                    <img src={instagramIcon} alt="Instagram" />
+                </a>
+                <a href="#" className="footer__icon">
+                    <img src={twitterIcon} alt="Twitter" />
+                </a>
+                
+                
+            
+                </div>
         </div>
     </footer>
   );
