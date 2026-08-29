ff755eb96730dd7699afefaebc3ffc21f45003cb
catalina <catalinaruedaf@hotmail.com>
2026-08-30 06:57:08 +1000

    compliting the styles for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,8 @@ export default function Footer() {
     <footer className="footer">
         <div className="footer__container container">
             <div className="footer__info ">
-                <h1 className="footer__title">Modern art gallery</h1>
+                <h1 className="footer__title">Modern 
+                   <span>Art Gallery</span> </h1>
 
                 <p className="footer__text">The Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9am.  Find us at 99 King Street, Newport, USA</p>
 
